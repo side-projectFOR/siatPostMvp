91da095bf44ada0e0959436205a7701a2c60ca33
정지훈 <mudokja@gmail.com>
2025-04-15 16:48:46 +0900

    fix: 어노테이션 누락 수정

--- src/main/java/com/siat/post/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/siat/post/domain/post/dto/PostRequestDto.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Builder
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class PostRequestDto {
     private Integer boardIdx;
